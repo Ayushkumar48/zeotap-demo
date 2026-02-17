a3d5dcd38a82b95af0a40887b96375f02b599c8f
Ayush Kumar <94519836+Ayushkumar48@users.noreply.github.com>
2026-02-17 11:51:49 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@zeotap-demo/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
